1b3eea7759baf460167e864179aef8e46ad94844
Your Name <you@example.com>
2017-08-16 14:04:14 -0500

    Hopefully this works

---
+++ Home/CustomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    class CustomList
+    {
+        public CustomList()
+        {
+
+        }
+    }//list<int i - new list<int>();
+    //i.add(16);
+    //i.add(20);
+    //custom list<int>list new customlist<int>();
+    //customList<lemon> Lemon = new CustomerList<lemons>();
+}
---
+++ Home/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}//index
+//use an add method
+//public void 
+//t[] items = new t[];
+
+
+    //list<int i - new list<int>();
+    //i.add(16);
+    //i.add(20);
+    //custom list<int>list new customlist<int>();
+    //customList<lemon> Lemon = new CustomerList<lemons>();
